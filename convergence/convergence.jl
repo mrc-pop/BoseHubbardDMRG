e38660b49ed21f2dc0ef17f3a9355b8d977498e9
Alessandro Gori <alegori2002@gmail.com>
2025-02-04 18:46:42 +0100

    progress on convergence analysis; a little bug to be solved in nsweeps convergence

--- convergence/convergence.jl
+++ convergence/convergence.jl
@@ -26,16 +26,15 @@ setting (L,N) and analyze eventual saturation of the observables.
 
 # TODO Print also maxlinkdim (screen printed)
 
-function RunConvergenceMaxM(ModelParameters::Vector{Float64},
+function RunConvergenceMaxM(MaxMFilePath::String,
+						    ModelParameters::Vector{Float64},
 							nSweeps::Int64,
 							MaxDims::Vector{Int64},
 							Cutoff::Float64)
 
 	# First subtest: nsweeps is fixed, investigate maxm
-	FilePathOut = PROJECT_ROOT * "/maxdim_data.txt"
 	
-	DataFile = open(FilePathOut,"w")
-	write(DataFile,"# Hubbard model DMRG. L=$L, N=$N, J=$J, μ=$μ, nmax=$nmax\n")
+	DataFile = open(MaxMFilePath,"a")
 	write(DataFile,"# maxm, E [calculated $(now()) @ nsweeps=$nSweeps]\n")
 
 	for SweepsMaxDim in MaxDims
@@ -49,57 +48,28 @@ function RunConvergenceMaxM(ModelParameters::Vector{Float64},
 
 	end
 	
-	# TODO Separate plot from simulations
-	
-	plot(size=(600,400), 
-	     xlabel=L"$\max_m$", ylabel=L"$E_g$",
-	     formatter = :plain,
-	     title=L"Energy after $%$nSweeps$ DMRG sweeps")
-	    
-	Data = readdlm(FilePathOut, ',', Float64, '\n'; comments=true)
-	xData = Data[:,1]
-	yData = Data[:,2]
-	scatter!(xData, yData, label=L"$\max_m$")
-	savefig(PROJECT_ROOT * "/maxdim_plot.pdf")
-	
 	return None
 end
 
-function RunConvergenceNSweeps(ModelParameters::Vector{Float64},
+function RunConvergenceNSweeps(NSweepsFilePath::String,
+							   ModelParameters::Vector{Float64},
 							   DMRGSweeps::Vector{Int64},
 							   MaxM::Int64,
 							   Cutoff::Float64)
 
 	# Second subtest: maxm is fixed, investigate nsweeps
 	
-	# File write
-	FilePathOut = PROJECT_ROOT * "/convergence/nsweeps_data.txt"
-	
-	DataFile = open(FilePathOut,"w")
-	write(DataFile,"# Hubbard model DMRG. L=$L, N=$N, J=$J, μ=$μ, nmax=$nmax\n")
+	DataFile = open(NSweepsFilePath,"a")
     write(DataFile,"# nsweeps, E [calculated $(now()) @ maxm=$MaxM]\n")
 
 	for nSweeps in DMRGSweeps
 	
-		DMRGParameters = [nSweeps, MaxM, cutoff]
-		E, _, _, _, _, _ = RunDMRGAlgorithm(ModelParameters, DMRGParameters; verbose=true)
+		DMRGParameters = [nSweeps, MaxM, Cutoff]
+		E, _, _, _, _, _, psi = RunDMRGAlgorithm(ModelParameters, DMRGParameters; verbose=true)
 		
 		write(DataFile,"$nSweeps, $E\n")
 
 	end
-	
-	# TODO Separate plot from simulations
-	
-	plot(size=(600,400), 
-	     xlabel=L"$n_\mathrm{sw}$", ylabel=L"$E_g$",
-	     formatter = :plain,
-	     title=L"Energy using $%$MaxM$ max bond link")
-	    
-	Data = readdlm(FilePathOut, ',', Float64, '\n'; comments=true)
-	xData = Data[:,1]
-	yData = Data[:,2]
-	scatter!(xData, yData, label=L"$n_\mathrm{sw}$")
-	savefig(PROJECT_ROOT * "/nsweeps_plot.pdf")
 	
 	return None
 end
@@ -115,20 +85,48 @@ function main()
 	ModelParameters = [L, N, nmax, J, μ]
 	
 	# TODO import DMRG parameters from user input 
-	Cutoff = [1E-12]
+	Cutoff = 1E-12
 
-	error_msg = "Input error: use option --maxm or --nsweeps"
+	ModeErrorMsg = "Input error: use option --maxm or --nsweeps"
+	RunErrorMsg = "Input error: set y or n"
+	Header = "# Hubbard model DMRG. L=$L, N=$N, J=$J, μ=$μ, nmax=$nmax\n"
 	
 	if length(ARGS) != 1
-		error(error_msg)
+		
+		# If user does not specify the user mode
+		error(ModeErrorMsg)
 		exit()
+		
 	else
+	
 		UserMode = ARGS[1]
+	
+		# Let the user decide to run simulations or not
+		print("Should I run simulations (already simulated data will be overwritten)? (y/n) ")
+		UserRun = readline()
+		if UserRun == "y"
+			run = true
+			println("Starting computation...")
+		elseif UserRun == "n"
+			run = false
+			println("Reading data...")
+		else
+			error(RunErrorMsg)
+			exit()
+		end
 		
 		if UserMode == "--maxm"
+
+			# First subtest: nsweeps is fixed, investigate maxm
+		
 			nSweeps = 5
 			MaxDims = [x for x in 10:10:100]	# Max dimensions to investigate
-			println("Running convergence simulations on parameter \"maxm\" using the following model:
+			MaxMFilePath = PROJECT_ROOT * "/maxdim_data.txt"
+			
+			if run
+				DataFile = open(MaxMFilePath,"w")
+				write(DataFile,Header)
+				println("Running convergence simulations on parameter \"maxm\" using the following model:
 · L = $L
 · N = $N
 · nmax = $nmax
@@ -138,13 +136,32 @@ and the DMRG setup:
 · nsweeps = $nSweeps
 · cutoff = $Cutoff
 The investigated values of maxm are: $MaxDims")
+				RunConvergenceMaxM(MaxMFilePath,ModelParameters,nSweeps,MaxDims,Cutoff)
+			end
 			
-			RunConvergenceMaxM(ModelParameters,nSweeps,MaxDims,Cutoff)
+			plot(size=(600,400), 
+				 xlabel=L"$\max_m$", ylabel=L"$E_g$",
+				 formatter = :plain,
+				 title=L"Energy after $%$nSweeps$ DMRG sweeps")
+
+			Data = readdlm(MaxMFilePath, ',', Float64, '\n'; comments=true)
+			xData = Data[:,1]
+			yData = Data[:,2]
+			scatter!(xData, yData, label=L"$\max_m$")
+			savefig(PROJECT_ROOT * "/maxdim_plot.pdf")
 			
 		elseif UserMode == "--nsweeps"
+		
+			# Second subtest: maxm is fixed, investigate nsweeps	
+		
 			MaxM = 200
 			DMRGSweeps = [x for x in 2:2:20]	# Number of sweeps to investigate
-			println("Running convergence simulations on parameter \"nsweeps\" using the following model:
+			NSweepsFilePath = PROJECT_ROOT * "/nsweeps_data.txt"
+			
+			if run
+				DataFile = open(NSweepsFilePath,"w")
+				write(DataFile,Header)
+				println("Running convergence simulations on parameter \"nsweeps\" using the following model:
 · L = $L
 · N = $N
 · nmax = $nmax
@@ -154,11 +171,22 @@ and the DMRG setup:
 · maxm = $MaxM
 · cutoff = $Cutoff
 The investigated values of nsweeps are: $DMRGSweeps")
-
-			RunConvergenceNSweeps(ModelParameters,DMRGSweeps,MaxM,Cutoff)
+				RunConvergenceNSweeps(NSweepsFilePath,ModelParameters,DMRGSweeps,MaxM,Cutoff)
+			end
+		
+			plot(size=(600,400), 
+				 xlabel=L"$n_\mathrm{sw}$", ylabel=L"$E_g$",
+				 formatter = :plain,
+				 title=L"Energy using $%$MaxM$ max bond link")
+				
+			Data = readdlm(NSweepsFilePath, ',', Float64, '\n'; comments=true)
+			xData = Data[:,1]
+			yData = Data[:,2]
+			scatter!(xData, yData, label=L"$n_\mathrm{sw}$")
+			savefig(PROJECT_ROOT * "/nsweeps_plot.pdf")
 			
 		else
-			error(error_msg)
+			error(ModeErrorMsg)
 			exit()
 		end
 	end
