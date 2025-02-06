c454ae7f7a52df2464bf4e411504bcba8efd0048
nepero27178 <alegori2002@gmail.com>
2025-02-06 11:40:17 +0100

    general efficiency improvement; redefinition of functions; partial push,still to finish in some parts

--- src/simulations.jl
+++ src/simulations.jl
@@ -13,96 +13,6 @@ PROJECT_ROOT *= "/.."	# Absloute path up to .../BoseHubbardDMRG/
 # --- Functions ---
 # -----------------
 
-function RectangularSweep(i::Int64,
-    					  L::Int64,
-    					  N::Int64,
-    					  nmax::Int64,
-    					  JJ::Vector{Float64},				# Horizontal sweep
-						  μμ::Vector{Float64},				# Vertical sweep
-    					  DMRGParameters::Vector{Any},
-    					  FilePathOut::String)
-    """
-    Calculate the variance of the number of particles on site i for a range of
-    hopping J and chemical potential μ values. Results are saved to a file.
-    """
-    DataFile = open(FilePathOut,"w")
-    write(DataFile,"# Hubbard model DMRG. L=$L, N=$N, nmax=$nmax, i=$i.\n")
-    write(DataFile,"# J, mu, n_variance [calculated $(now())]\n")
-
-    # vars = zeros(length(μμ), length(JJ))
-
-    for J in JJ
-        for μ in μμ
-            ModelParameters = [L, N, nmax, J, μ]
-            _, _, _, nVariance, _, Γ, _ = RunDMRGAlgorithm(ModelParameters,
-                                            DMRGParameters; FixedN = false)
-            write(DataFile,"$J, $μ, $(nVariance[i])\n")
-        end
-    end
-
-    close(DataFile)
-    println("Data saved on file!")
-end
-
-# function CalculatePhaseBoundaries(LL::Array{Int64}, JJ::Array{Float64},
-#     nmax::Int64, μ0::Float64, DMRGParameters, FilePathOut)
-#     """
-#     Calculate the phase boundaries between the Mott Insulator (MI) and 
-#     Superfluid (SF) phases, for the first lobe (N=L, meaning ρ=1).
-#     The results are saved to a file.
-#     """
-
-#     DataFile = open(FilePathOut,"w")
-#     write(DataFile,"# Hubbard model DMRG. nmax=$nmax, μ0=$μ0.\n")
-#     write(DataFile,"# L, J, DeltaE_g^+, DeltaE_g^- [calculated $(now())]\n")
-
-#     E = zeros(Float64, 3)
-
-#     for L in LL
-#         println("Processing L = $L")
-#         for J in JJ
-#             E[1], _, _, _, _, _ = RunDMRGAlgorithm([L, L-1, nmax, J, μ0], 
-#                                                     DMRGParameters; FixedN = true)
-#             E[2], _, _, _, _, _ = RunDMRGAlgorithm([L, L, nmax, J, μ0], 
-#                                                     DMRGParameters; FixedN = true)
-#             E[3], _, _, _, _, _ = RunDMRGAlgorithm([L, L+1, nmax, J, μ0], 
-#                                                     DMRGParameters; FixedN = true)
-#             ΔEplus = E[3] - E[2]
-#             ΔEminus = E[2] - E[1]
-#             write(DataFile,"$L, $J, $ΔEplus, $ΔEminus\n")
-#         end
-#     end
-#     close(DataFile)
-#     println("Data saved on file!")
-# end
-
-# function CalculateCorrelationFunction(LL::Array{Int64}, JJ::Array{Float64},
-#     nmax::Int64, DMRGParameters, FilePathOut::String; pbc=false)
-#     """
-#     Calculate Γ(r) for various L and J. Save data on file.
-#     """
-
-#     μ = 1 # SF phase
-
-#     DataFile = open(FilePathOut,"w")
-#     write(DataFile,"# Hubbard model DMRG. nmax=$nmax, μ=$μ (SF phase).\n")
-#     write(DataFile,"# J, L, Γ(r) [r even, r<L/2] [calculated $(now())]\n")
-
-#     for J in JJ
-#         for L in LL
-#             N = L # arbitrary
-#             println("Calculating correlation functions for L=$L, J=$J.")
-#             _, _, _, _, _, Γ = RunDMRGAlgorithm([L, N, nmax, J, μ], DMRGParameters; 
-#             calculate_gamma=true, FixedN=true, pbc=false)
-#             write(DataFile,"$J; $L; $Γ\n")
-#         end
-#     end
-
-#     close(DataFile)
-#     println("Correlation functions saved to: ", FilePathOut)
-# end
-
-
 function HorizontalSweep(LL::Array{Int64},
 						 nmax::Int64,
 						 JJ::Array{Float64},
@@ -134,21 +44,67 @@ function HorizontalSweep(LL::Array{Int64},
            	# Different filling simulations; ComputeAllObservables=false by default
             E[1], _, _, = RunDMRGAlgorithm([L, L-1, nmax, J, μ0], 
                                     	   DMRGParameters;
-                                    	   ComputeGamma=true, 
-		                                   FixedN = true)
+                                    	   FixedN = true)
             E[2], nVariance, Γ, = RunDMRGAlgorithm([L, L, nmax, J, μ0], 
                                             	   DMRGParameters;
 				                            	   ComputeGamma=true, 
 						                           FixedN = true)
             E[3], _, _, = RunDMRGAlgorithm([L, L+1, nmax, J, μ0], 
-                                    	   DMRGParameters;
-                                    	   ComputeGamma=true, 
+                                    	   DMRGParameters; 
 		                                   FixedN = true)
             ΔEplus = E[3] - E[2]
             ΔEminus = E[2] - E[1]
-            write(DataFile,"$L; $J; $(E[2]); $ΔEplus; $ΔEminus; $nVariance; $Γ\n")
+
+            μplus = ΔEplus + μ0
+            μminus = -ΔEminus + μ0
+
+            write(DataFile,"$L; $J; $(E[2]); $μplus; $μminus; $nVariance; $Γ\n")
         end
     end
+    close(DataFile)
+    println("Data saved on file!")
+end
+
+function RectangularSweep(i::Int64,
+    					  L::Int64,
+    					  N::Int64,
+    					  nmax::Int64,
+    					  JJ::Vector{Float64},				# Horizontal sweep
+						  μμ::Vector{Float64},				# Vertical sweep
+    					  DMRGParametersMI::Vector{Any},
+    					  DMRGParametersSF::Vector{Any},
+    					  FilePathIn::String,				# To evaluate if a given point is MI or SF
+    					  FilePathOut::String)
+    """
+    Calculate the variance of the number of particles on site i for a range of
+    hopping J and chemical potential μ values. Results are saved to a file.
+    """
+    
+    DataFile = open(FilePathOut,"w")
+    write(DataFile,"# Hubbard model DMRG. L=$L, N=$N, nmax=$nmax\n")
+    write(DataFile,"# NOTE: Different DMRG settings have been used for MI and SF. Check the code.\n")
+    write(DataFile,"# J, μ, E, n_variance [calculated $(now()) @site  i=$i]\n")
+
+	# TODO Generate file from fitting data of horizontal sweeps to separate MI from SF
+	# TODO Read data from file
+
+    for J in JJ
+        for μ in μμ
+            ModelParameters = [L, N, nmax, J, μ]
+
+			if J is MI # TODO finish
+	            E, nVariance, _ = RunDMRGAlgorithm(ModelParameters,
+	                                               DMRGParametersMI;
+	                                               FixedN = false)
+	            write(DataFile,"$J, $μ, $E, $(nVariance[i]) # MI\n")
+	        elseif J is SF # TODO finish
+	        	E, nVariance, _ = RunDMRGAlgorithm(ModelParameters,
+	                                               DMRGParametersSF;
+  		                                           FixedN = false)
+	            write(DataFile,"$J, $μ, $E, $(nVariance[i]) # SF\n")
+        end
+    end
+
     close(DataFile)
     println("Data saved on file!")
 end
@@ -157,32 +113,58 @@ function main()
 
     # TODO Import DMRG parameters from user input
     nmax = 3
+
+    # Mott Insulator DMRG parameters
     nsweeps = 5
-    maxlinkdim = [10,50,75,200,500]		# TODO Change
-    cutoff = [1E-12]					# TODO Change
-    DMRGParameters = [nsweeps, maxlinkdim, cutoff]
+    maxlinkdim = [10,50,75,200,500]		# TODO Change with optimal values
+    cutoff = [1E-12]					# TODO Change with optimal values
+    DMRGParametersMI = [nsweeps, maxlinkdim, cutoff]
 
-    # ----------------------
-    # --- Study Variance ---
-    # ----------------------
-    # N = 10
-    # L = 10
-    # JJ = collect(0.0:0.03:0.3)
-    # μμ = collect(range(0.0,1,5))
-    # i = ceil(Int64, L/2) # site on which to calculate variance
-    # CalculateVariance(i, L, N, nmax, JJ, μμ, DMRGParameters,
-    #    "../simulations/data_variance.txt")
+	# Superfluid phase DMRG paramters
+	nsweeps = 5
+	maxlinkdim = [10,50,75,200,500]		# TODO Change with optimal values
+	cutoff = [1E-12]					# TODO Change with optimal values
+	DMRGParametersSF = [nsweeps, maxlinkdim, cutoff]
+    
+	ModeErrorMsg = "Input error: use option --horizontal or --rectangular"
+	
+	if length(ARGS) != 1
+		# If user does not specify the user mode
+		error(ModeErrorMsg)
+		exit()
+	else
+		
+		UserMode = ARGS[1]
+		    
+		if UserMode=="--horizontal"
 
-    # -----------------------------------------------------------
-    # --- Boundary between SF and MI & Correlation function Γ ---
-    # -----------------------------------------------------------
-    
-    # TODO Import model parameters from user input
-    JJ = collect(range(start=0.0, stop=0.35, length=100))
-    LL = [10, 20, 30]
-    
-    FilePathOut = PROJECT_ROOT * "/simulations/simulations_240204.txt")
-    CalculateObservables(LL, JJ, nmax, DMRGParameters, FilePathOut)
+			# Horizontal sweep
+	    	# TODO Import model parameters from user input
+	    	JJ = collect(range(start=0.0, stop=0.35, length=100))
+	    	LL = [10, 20, 30]
+	    
+	    	FilePathOut = PROJECT_ROOT * "/simulations/L=$L/horizontal_sweep.txt")
+	    	# TODO Evaluate: use superfluid DMRG paramters?
+	    	HorizontalSweep(LL, JJ, nmax, DMRGParametersSF, FilePathOut)
+
+		if UserMode=="--rectangular"
+
+			# Rectangular sweep
+		    N = 10 								# TODO Change
+		    L = 10 								# TODO Change
+		    JJ = [J for J in 0.0:0.03:0.3]		# TODO Change, exclude J=0
+		    μμ = [μ for μ in 0.0:0.1:1.0]		# TODO Change
+
+		    # TODO Cycle over sites, extend to different fillings
+		    i = ceil(Int64, L/2) # site on which to calculate variance
+			FilePathOut = PROJECT_ROOT * "/simulations/L=$L/rectangular_sweep_site=$i"
+		    RectangularSweep(i, L, N, nmax, JJ, μμ, DMRGParametersMI, DMRGParametersSF, FilePathOut)
+		
+		else
+			error(ModeErrorMsg)
+			exit()
+		end
+	end
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
